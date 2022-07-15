a1a300d3e7ff85f394194f801939d393d4d02cc8
ReverieNB <62551793+ReverieNB@users.noreply.github.com>
2022-07-15 11:21:09 -0500

    Initial functions

---
+++ R/misc.R
@@ -0,0 +1,5 @@
+'%ni%' <- Negate('%in%')
+
+raw <- "/dbfs/mnt/phmdw/Raw/PublicHealth/"
+refined <- "/dbfs/mnt/phmdw/Refined/PublicHealth/"
+trusted <- "/dbfs/mnt/phmdw/Trusted/PublicHealth/"
---
+++ Update package.R
@@ -0,0 +1,12 @@
+#Use this script to update the package documentation when new functions are added
+#there's probably a newer way do this, but it's always worked fine for me
+#https://hilaryparker.com/2014/04/29/writing-an-r-package-from-scratch/
+
+library(devtools)
+library(roxygen2)
+
+document()
+
+#once the documentation has been updated, upload the new files to the github folder
+#then can download the updated package with:
+#devtools::install_github('PHinfo','ReverieNB')
