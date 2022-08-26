833300dd6ac88094147601c421c8941a8076791f
ReverieNB <62551793+ReverieNB@users.noreply.github.com>
2022-08-26 09:39:34 -0500

    New functions

--- R/misc.R
+++ R/misc.R
@@ -1,25 +1,112 @@
+#' Not-in Operator
+#'
+#' Commonly used default colors
+#'
+#' @export
+
 '%ni%' <- Negate('%in%')
 
-#data lake file paths
+#' Data lake folder path.
+#'
+#' Raw folder path
+#'
+#' @export
+
 raw <- "/dbfs/mnt/phmdw/Raw/PublicHealth/"
+
+#' Data lake folder path.
+#'
+#' Refined folder path
+#'
+#' @export
+
 refined <- "/dbfs/mnt/phmdw/Refined/PublicHealth/"
+
+#' Data lake folder path.
+#'
+#' Trusted folder path
+#'
+#' @export
+
 trusted <- "/dbfs/mnt/phmdw/Trusted/PublicHealth/"
+
+#' Data lake folder path.
+#'
+#' COVID data lake path
+#'
+#' @export
+
 covid <- "/dbfs/mnt/covid/"
 
-#standard color palettes
-#from: https://www.hennepin.us/-/media/hennepin-branding/data-visualization-guide-march-2021.pdf
+#' HC Sequential Color Palette.
+#'
+#' Commonly used default colors
+#'
+#' @export
 
 hc_sequential <- c("#c6e8fb", "#88bfe7", "#4895c8", "#22689a", "#113c66", "#dbcae3", "#bfa2cd", "#9e76b4", "#745195", "#543079")
 
+#' HC Categorical Color Palette.
+#'
+#' Commonly used default colors
+#'
+#' @export
+
 hc_categorical <- c("#005aaa", "#6dcff6", "#113c66", "#f7931d", "#543079", "#ffcb04", "#5ba345")
+
+#' HC Diverging Color Palette.
+#'
+#' Commonly used default colors
+#'
+#' @export
 
 hc_diverging <- c("#f7931d", "#ffcb04", "#dfe0de", "#bfa2cd", "#9e76b4", "#543079", "#5ba345", "#9ecb3a", "#dfe0de", "88bfe7",
                   "#4896c8", "#113c66")
 
-#color palettes I used in the past
+#' Base Blue.
+#'
+#' Commonly used default colors
+#'
+#' @export
+
 base_blue <- "#1f78b4"
+
+#' Base Two Color Palette.
+#'
+#' Commonly used default colors
+#'
+#' @export
+
 base_two <- c("#1f78b4","#b2df8a")
+
+#' Base Four Color Palette.
+#'
+#' Commonly used default colors
+#'
+#' @export
+
 base_four <- c("#482677ff", "#2d708eff", "#20a387ff","#73d055ff")
+
+#' Base Five Color Palette.
+#'
+#' Commonly used default colors
+#'
+#' @export
+
 base_five <- c('#f0f9e8','#bae4bc','#7bccc4','#43a2ca','#0868ac')
+
+#' Default Missing Color
+#'
+#' Commonly used default colors
+#'
+#' @export
+
 na_color <- "#D0D0D0"
+
+#' Default Red Heat Palette
+#'
+#' Commonly used default colors
+#'
+#' @export
+
 heat_pal <- c("#FEF0D9", "#FDCC8A", "#FC8D59", "#E34A33", "#B30000") #from RCB OrRD, expand as needed
