9aeebdd76eb580290bdda3cfb016ac2c75cb9b2f
ReverieNB <62551793+ReverieNB@users.noreply.github.com>
2022-09-26 11:14:31 -0500

    added clean_match

---
+++ R/clean_match.R
@@ -0,0 +1,16 @@
+#' Clean data before probabilistic matching.
+#'
+#' Clean first and last name fields before matching with RecordLinkage or other tools.
+#' @param x a dataframe
+#' @param first name of variable with first name
+#' @param last name of variable with first name
+#' @export
+
+
+clean_match <- function(x, first = "first_name", last = "last_name"){
+  x %>%
+    mutate(dplyr::across(c(tidyselect::all_of(first), tidyselect::all_of(last)), toupper),
+           dplyr::across(c(tidyselect::all_of(first), tidyselect::all_of(last)), ~stringr::str_remove_all(.x, "[[:punct:]]")),
+           dplyr::across(c(tidyselect::all_of(first), tidyselect::all_of(last)), ~stringr::str_remove_all(.x, " JR| IV| III| II")), #include SR?
+           dplyr::across(c(tidyselect::all_of(first), tidyselect::all_of(last)), ~stringr::str_remove_all(.x, " ")))
+}
