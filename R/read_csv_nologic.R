b3cc052e6e05c8fede6c9c8e4b0238cffea42578
ReverieNB <62551793+ReverieNB@users.noreply.github.com>
2022-10-17 11:34:36 -0500

    new read csv function

---
+++ R/read_csv_nologic.R
@@ -0,0 +1,22 @@
+#' Read csv files.
+#'
+#' Read csv files, and convert all columns that would be read in as logical as character. This prevents losing information in columns that are mostly blank.
+#' @param x filepath
+#' @export
+
+read_csv_nologic <- function(x, ...){ #this will be a slow function because things are being read in twice
+
+  #get list of all logical columns
+  logic <- read_csv(x, ...) %>%
+    select(where(is.logical))
+
+  fixes <- list(rep("c", length(names(logic))))
+  names(fixes[[1]]) = names(logic)
+
+  #read in file, correct logical column types
+  temp <- read_csv(x,
+                   col_types = fixes[[1]],
+                   ...)
+
+  return(temp)
+}
