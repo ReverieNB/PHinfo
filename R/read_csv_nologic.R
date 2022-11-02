272fdea8fc7e0531d55a39a460e25ff64047dd9e
ReverieNB <62551793+ReverieNB@users.noreply.github.com>
2022-11-02 09:55:22 -0500

    new read_xlsx function

--- R/read_csv_nologic.R
+++ R/read_csv_nologic.R
@@ -7,14 +7,14 @@
 read_csv_nologic <- function(x, ...){ #this will be a slow function because things are being read in twice
 
   #get list of all logical columns
-  logic <- read_csv(x, ...) %>%
-    select(where(is.logical))
+  logic <- readr::read_csv(x, ...) %>%
+    dplyr::select(where(is.logical))
 
   fixes <- list(rep("c", length(names(logic))))
   names(fixes[[1]]) = names(logic)
 
   #read in file, correct logical column types
-  temp <- read_csv(x,
+  temp <- readr::read_csv(x,
                    col_types = fixes[[1]],
                    ...)
 
---
+++ R/read_xlsx_nologic.R
@@ -0,0 +1,23 @@
+#' Read xlsx files.
+#'
+#' Read xlsx files, and convert all columns that would be read in as logical as character. This prevents losing information in columns that are mostly blank.
+#' @param x filepath
+#' @export
+
+read_xlsx_nologic <- function(x, ...){ #this will be a slow function because things are being read in twice
+
+  #get list of all column types, adjust logical types
+  fixes <- readxl::read_xlsx(x, ...) %>%
+    purrr::map_df(class) %>%
+    dplyr::slice(1) %>%
+    dplyr::mutate(dplyr::across(everything(), ~ ifelse(. == "logical", "text", "guess"))) %>%
+    purrr::as_vector() %>%
+    unname()
+
+  #read in file, correct logical column types
+  temp <- readxl::read_xlsx(x,
+                           col_types = fixes,
+                           ...)
+
+  return(temp)
+}
