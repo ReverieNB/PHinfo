cebea9967add50e44f308447086b3fa64efbcf0e
ReverieNB <62551793+ReverieNB@users.noreply.github.com>
2023-03-15 14:07:41 -0500

    read delim function

---
+++ R/read_delim_nologic.R
@@ -0,0 +1,24 @@
+#' Read delimited files.
+#'
+#' Read delimited files, and convert all columns that would be read in as logical as character. This prevents losing information in columns that are mostly blank.
+#' @param x filepath
+#' @export
+
+read_delim_nologic <- function(x, ...){ #this will be a slow function because things are being read in twice
+
+  #get list of all logical columns
+  logic <- readr::read_delim(x,
+                           n_max = 1001,
+                           ...) %>%
+    dplyr::select(where(is.logical))
+
+  fixes <- list(rep("c", length(names(logic))))
+  names(fixes[[1]]) = names(logic)
+
+  #read in file, correct logical column types
+  temp <- readr::read_delim(x,
+                          col_types = fixes[[1]],
+                          ...)
+
+  return(temp)
+}
