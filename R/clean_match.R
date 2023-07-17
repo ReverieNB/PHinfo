a33b461c6c1910cc65e3883f9f6864f25e2f9563
ReverieNB <62551793+ReverieNB@users.noreply.github.com>
2023-07-17 14:37:24 -0500

    Update clean_match.R

--- R/clean_match.R
+++ R/clean_match.R
@@ -7,23 +7,26 @@
 #' @param multi_flag will flag last names that appear to be multi-part for use later in the matching process
 #' @export
 
-#One common issue is that people who have a multi-part/hypenated last name in one data source receive a lower score than is reasonable
-#using common sense to the algo needs to make quite a few changes to add an entire surname. Especially the case when the new part is
-#quite long. Can use the multi_flag option to ID when this may be the case, so weights can be manually adjusted if necessary. See matching
-#code for how this is used, but essentially if the flag exists in one dataset but not the other I will check to see if the complete single name
+#One common issue is that people who have a multi-part/hypenated last name in one data source receive a lower match weight than is reasonable
+#using common sense because the algo needs to make quite a few changes to add an entire new surname. Especially the case when the new part is
+#quite long. Can use the multi_flag option to ID when this may be the case, so weights can be manually adjusted if necessary.
+#Results will be flagged if any of these conditions is true:
+#1) There is a space somewhere in the name, 2) there is a hyphen in the name, 3) There are multiple uppercase letters separated by at least 3 lowercase letters
+#See adjust_match() for how this is used, but essentially if the flag exists in one dataset but not the other it will check to see if the complete single name
 #makes up either the beginning or end of the multipart name. If so, adjust up to a minimum value such as .9. Could condition this on first name being
 #a perfect match.
 
 clean_match <- function(x, first = "first_name", last = "last_name", multi_flag = FALSE){
   temp <- x |>
-    dplyr::mutate(last_flag = ifelse(str_detect(!!sym(last), "\\-|^[:upper:]...*[:upper:]"), 1, 0),
+    dplyr::mutate(last_flag = ifelse(stringr::str_detect(!!sym(last), "\\-|^[:upper:]...*[:upper:]"), 1, 0),
                   dplyr::across(c(tidyselect::all_of(first), tidyselect::all_of(last)), ~stringr::str_remove_all(.x, "[[:punct:]]")),
                   dplyr::across(c(tidyselect::all_of(first), tidyselect::all_of(last)), toupper),
                   dplyr::across(c(tidyselect::all_of(first), tidyselect::all_of(last)), ~stringr::str_remove_all(.x, " JR| IV| III| II | SR$")),
-                  last_flag = ifelse(str_detect(!!sym(last), "\\s"), 1, last_flag),
+                  {{last}} := stringr::str_trim(!!sym(last)),
+                  last_flag = ifelse(stringr::str_detect(!!sym(last), "\\s"), 1, last_flag),
                   dplyr::across(c(tidyselect::all_of(first), tidyselect::all_of(last)), ~stringr::str_remove_all(.x, " ")))
 
-  if (flag_multi == FALSE){
+  if (multi_flag == FALSE){
 
     return(temp |> dplyr::select(-last_flag))
 
