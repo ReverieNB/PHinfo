f6e8c3254941abc0300de7ac2c6f71eadb9b0d1b
ReverieNB <62551793+ReverieNB@users.noreply.github.com>
2023-07-17 15:50:26 -0500

    Update clean_match.R

--- R/clean_match.R
+++ R/clean_match.R
@@ -18,7 +18,7 @@
 
 clean_match <- function(x, first = "first_name", last = "last_name", multi_flag = FALSE){
   temp <- x |>
-    dplyr::mutate(last_flag = ifelse(stringr::str_detect(!!sym(last), "\\-|^[:upper:]\\w\\w\\w*[:upper:]"), 1, 0),
+    dplyr::mutate(last_flag = ifelse(stringr::str_detect(!!sym(last), "\\-|^[:upper:]\\w\\w\\w+[:upper:]\\w\\w\\w+"), 1, 0),
                   dplyr::across(c(tidyselect::all_of(first), tidyselect::all_of(last)), ~stringr::str_remove_all(.x, "[[:punct:]]")),
                   dplyr::across(c(tidyselect::all_of(first), tidyselect::all_of(last)), toupper),
                   dplyr::across(c(tidyselect::all_of(first), tidyselect::all_of(last)), ~stringr::str_remove_all(.x, " JR| IV| III| II| SR$")),
