a4fff34769fc9c6c5201a4805273e97340bab0e8
ReverieNB <62551793+ReverieNB@users.noreply.github.com>
2023-07-17 15:42:15 -0500

    Update clean_match.R

--- R/clean_match.R
+++ R/clean_match.R
@@ -18,10 +18,10 @@
 
 clean_match <- function(x, first = "first_name", last = "last_name", multi_flag = FALSE){
   temp <- x |>
-    dplyr::mutate(last_flag = ifelse(stringr::str_detect(!!sym(last), "\\-|^[:upper:]...*[:upper:]"), 1, 0),
+    dplyr::mutate(last_flag = ifelse(stringr::str_detect(!!sym(last), "\\-|^[:upper:]\\w\\w\\w*[:upper:]"), 1, 0),
                   dplyr::across(c(tidyselect::all_of(first), tidyselect::all_of(last)), ~stringr::str_remove_all(.x, "[[:punct:]]")),
                   dplyr::across(c(tidyselect::all_of(first), tidyselect::all_of(last)), toupper),
-                  dplyr::across(c(tidyselect::all_of(first), tidyselect::all_of(last)), ~stringr::str_remove_all(.x, " JR| IV| III| II | SR$")),
+                  dplyr::across(c(tidyselect::all_of(first), tidyselect::all_of(last)), ~stringr::str_remove_all(.x, " JR| IV| III| II| SR$")),
                   {{last}} := stringr::str_trim(!!sym(last)),
                   last_flag = ifelse(stringr::str_detect(!!sym(last), "\\s"), 1, last_flag),
                   dplyr::across(c(tidyselect::all_of(first), tidyselect::all_of(last)), ~stringr::str_remove_all(.x, " ")))
