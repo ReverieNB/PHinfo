9df93de725b87342468e6289b91f7500be491bd3
ReverieNB <62551793+ReverieNB@users.noreply.github.com>
2023-07-18 10:20:22 -0500

    Update adj_name_wt.R

--- R/adj_name_wt.R
+++ R/adj_name_wt.R
@@ -12,11 +12,11 @@ adj_name_wt <- function(data, wt = 0.9){
     dplyr::mutate(
       #adjust weight if one is a single name, one is multi-part, plus the single name is at either the beginning or end of the multi-part name
       weight_adj = dplyr::case_when(
-        last_flag_1 == 1 & last_flag_2 == 0 & stringr::str_detect(last_name_1, paste0("^", last_name_2, "|", last_name_2, "$")) ~ as.numeric(wt),
-        last_flag_1 == 0 & last_flag_2 == 1 & stringr::str_detect(last_name_2, paste0("^", last_name_1, "|", last_name_1, "$")) ~ as.numeric(wt),
+        last_flag.1 == 1 & last_flag.2 == 0 & stringr::str_detect(last_name.1, paste0("^", last_name.2, "|", last_name.2, "$")) ~ as.numeric(wt),
+        last_flag.1 == 0 & last_flag.2 == 1 & stringr::str_detect(last_name.2, paste0("^", last_name.1, "|", last_name.1, "$")) ~ as.numeric(wt),
         TRUE ~ 0),
       #for now, use the logic that in order for an adjustment the first names need to match perfectly, and adjusted weight > initial weight
-      adj_flag = ifelse(first_name_1 == first_name_2 & weight_adj == wt & weight_adj > Weight, 1, 0), #flag for adjusted weights
+      adj_flag = ifelse(first_name.1 == first_name.2 & weight_adj == wt & weight_adj > Weight, 1, 0), #flag for adjusted weights
       Weight = ifelse(adj_flag == 1, weight_adj, Weight)) |>
     dplyr::select(-weight_adj)
 
