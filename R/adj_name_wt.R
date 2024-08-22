b88f01964fc7006336be9909bbd7d81782cdca3d
ReverieNB <nathanabean@gmail.com>
2024-08-22 13:03:52 -0500

    Adding updated files

--- R/adj_name_wt.R
+++ R/adj_name_wt.R
@@ -1,12 +1,23 @@
 #' Adjust probabilistic matching weights.
 #'
-#' Adjust the weight of matching in instances where one dataset has a multi-part name and the other doesn't. This can be used to make up for the relative under weighting of these matches.
-#' @param data a dataframe created by matching two datasets
+#' Adjust the weight of matching in instances where one data set has a multi-part name and the other doesn't. This can be used to make up for the relative under weighting of these matches.
+#' @param data a dataframe created by matching two data sets
 #' @param wt the weight affected matches will be assigned if greater than the original weight.
 #' @export
 
-#Use this function after the probablistic match using names has been completed. May need to rename the name columns if they are not already in the 'first_name' format.
+#Use this function after the probabilistic match using names has been completed. May need to rename the name columns if they are not already in the 'first_name' format.
 adj_name_wt <- function(data, wt = 0.9){
+  
+  #If names have been cleaned, transform them back. Very inelegant, but a good enough fix. This isn't used much.
+  if ("last_flag_1" %in% names(data)){
+    data <- data |>
+      rename(last_flag.1 = last_flag_1, last_flag.2 = last_flag_2, 
+             last_name.1 = last_name_1, last_name.2 = last_name_2,
+             first_name.1 = first_name_1, first_name.2 = first_name_2, 
+             Weight = weight)
+    
+    clean_flag = 1
+  }
 
   temp <- data |>
     dplyr::mutate(
@@ -20,7 +31,15 @@ adj_name_wt <- function(data, wt = 0.9){
       Weight = ifelse(adj_flag == 1, weight_adj, Weight)) |>
     dplyr::select(-weight_adj)
 
-  print(paste0("Weight for ", sum(temp$adj_flag), " match", ifelse(sum(temp$adj_flag) == 1, "", "es"), " adjusted."))
+  message(paste0("Weight for ", sum(temp$adj_flag), " match", ifelse(sum(temp$adj_flag) == 1, "", "es"), " adjusted."))
+  
+  if (clean_flag == 1){
+    temp <- temp |>
+      rename(last_flag_1 = last_flag.1, last_flag_2 = last_flag.2, 
+             last_name_1 = last_name.1, last_name_2 = last_name.2,
+             first_name_1 = first_name.1, first_name_2 = first_name.2,
+             weight = Weight)
+  }
 
   return(temp)
 }
